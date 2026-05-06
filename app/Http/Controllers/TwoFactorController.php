c4f7d949dc628efc8725669956eb615371575c30
Yann <yann232yann>
2026-05-06 11:23:54 +0000

    first commit

--- app/Http/Controllers/TwoFactorController.php
+++ app/Http/Controllers/TwoFactorController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
+
 class TwoFactorController extends Controller
 {
     // ── Envoyer le code ───────────────────────────────────────────
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Mindscribe',
             'email' => 'test@example.com',
         ]);
     }
